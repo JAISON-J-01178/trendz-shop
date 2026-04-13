81b5559c6b016b200f467004bcd49d0da14a355f
JAISON-J-01178 <jk8730382@gmail.com>
2026-04-13 22:21:42 +0530

    Create getProducts.js

---
+++ api/getProducts.js
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default async function handler(req, res) {
+  try {
+    const result = await pool.query("SELECT * FROM products");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
